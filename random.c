c118b1168c650d5360f3db221b5751d34ac742d0
TonyGao <tuo60854@temple.edu>
2023-08-30 11:23:17 -0400

    a message

--- random.c
+++ random.c
@@ -1,2 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <ctype.h>
 
-
+char randchar() {
+   char alphabet[] = "abcdefghijklmnopqrstuvwxyz";
+   char letter = toupper(alphabet[rand()%26]);
+   return letter;
+}
